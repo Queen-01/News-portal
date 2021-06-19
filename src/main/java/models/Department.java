2b61d47473104f747181b10d18db189fa1723425
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-06-19 22:59:39 +0300

    add dao

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,34 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
+    public int id;
+    public String name;
+    public String description;
+
+    public Department(int id, String name, String description){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public boolean equals(Object o){
+        if (this == o)
+            return true;
+        if(o == null || getClass() != o.getClass())
+            return false;
+        Department that = (Department) o;
+        return id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description);
+    }
 }
