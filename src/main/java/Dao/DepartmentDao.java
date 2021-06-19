6563bf55c1da9d793bc26ba113c4b15a6ab5736c
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-06-19 23:16:31 +0300

    add news

--- src/main/java/Dao/DepartmentDao.java
+++ src/main/java/Dao/DepartmentDao.java
@@ -1,4 +1,13 @@
 package Dao;
 
+import models.DepartNews;
+import models.Department;
+import models.User;
+
+import java.util.List;
+
 public interface DepartmentDao {
+    List<Department>getAllDepart();
+    List<User>getDepartUserById();
+    List<DepartNews>getDepartNewsById();
 }
