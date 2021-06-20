8acecc240e1532b8fa58c593c6982464fd41399a
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-06-20 12:54:17 +0300

    add user file

---
+++ src/main/java/Dao/Sql2oUserDao.java
@@ -0,0 +1,37 @@
+package Dao;
+
+import models.User;
+
+import java.util.List;
+
+public class Sql2oUserDao implements UserDao{
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public User findUserById(int id) {
+        return null;
+    }
+
+    @Override
+    public Void update() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
+}
--- src/main/java/Dao/UserDao.java
+++ src/main/java/Dao/UserDao.java
@@ -1,4 +1,14 @@
 package Dao;
 
+import models.User;
+
+import java.util.List;
+
 public interface UserDao {
+    List<User>getAllUsers();
+    void addUser (User user);
+    User findUserById(int id);
+    Void update();
+    void deleteById(int id);
+    void clearAll();
 }
