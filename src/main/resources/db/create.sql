86028b52c1592c0db0a5fb7c07a440362d99f098
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-06-24 17:25:44 +0300

    made changes

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,6 +1,23 @@
 SET MODE PostgresSQL;
 CREATE DATABASE news_portal;
-CREATE TABLE department (id int PRIMARY KEY, name varchar, description varchar);
-CREATE TABLE users (id int PRIMARY KEY, name varchar, role varchar, position varchar, departId int);
-CREATE TABLE news (id int PRIMARY KEY, userId int, content varchar,posdate timestamp, departId int, type varchar);
+CREATE TABLE IF NOT EXISTS department (
+    id serial PRIMARY KEY,
+    name varchar,
+    description varchar
+);
+CREATE TABLE IF NOT EXISTS users (
+     id serial PRIMARY KEY,
+    name varchar,
+    role varchar,
+    position varchar,
+    departId int
+);
+CREATE TABLE IF NOT EXISTS news (
+    id serial PRIMARY KEY,
+    userId int,
+    content varchar,
+    posdate timestamp,
+    departId int,
+    type varchar
+);
 CREATE DATABASE news_portal_test WITH TEMPLATE news_portal;
